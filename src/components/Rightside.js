b7fc65d8ea5804931b67f732f7f28b0f0117e878
maniabhishek <maniabhishek05@gmail.com>
2021-05-12 02:49:26 +0530

    home pages in progress

---
+++ src/components/Rightside.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Rightside = (props) =>{
+    return (
+        <Container>
+            Leftside
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    grid-area:rightside;
+`;
+
+export default Rightside;
