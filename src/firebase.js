fcedbabb7626dcba913763c119e867c3ac06585f
maniabhishek <maniabhishek05@gmail.com>
2021-05-16 23:56:05 +0530

    firebase.js update

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyBUF40knK_eiJIVwTNwWA8uCF08crrUfuY",
-    authDomain: "linkedinclone-7bf22.firebaseapp.com",
-    projectId: "linkedinclone-7bf22",
-    storageBucket: "linkedinclone-7bf22.appspot.com",
-    messagingSenderId: "411312191540",
-    appId: "1:411312191540:web:c605b43696d98c43667261"
-  };
+  apiKey: "AIzaSyBUF40knK_eiJIVwTNwWA8uCF08crrUfuY",
+  authDomain: "linkedinclone-7bf22.firebaseapp.com",
+  projectId: "linkedinclone-7bf22",
+  storageBucket: "linkedinclone-7bf22.appspot.com",
+  messagingSenderId: "411312191540",
+  appId: "1:411312191540:web:ceee8b6436edf505667261"
+};
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
